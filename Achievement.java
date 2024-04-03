1764c9569d5012abc3ed20e201819c54f0f342c0
Vanessa-Huo <350170783@gapps.yrdsb.ca>
2024-04-03 09:04:54 -0400

    Created Worlds

---
+++ Achievement.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Achievement here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Achievement extends World
+{
+
+    /**
+     * Constructor for objects of class Achievement.
+     * 
+     */
+    public Achievement()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
