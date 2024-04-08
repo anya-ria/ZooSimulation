71d15a01da949820f150d15e64dd7d9305f6d336
Vanessa-Huo <350170783@gapps.yrdsb.ca>
2024-04-08 09:00:58 -0400

    Fixed conflicts

--- MyWorld.java
+++ MyWorld.java
@@ -18,15 +18,11 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 800, 1); 
-<<<<<<< HEAD
         Greenfoot.setSpeed(50);
     }
     
     
     public void act(){
-        
-=======
         addObject(new Regular(), 512, 400);
->>>>>>> 91db7f7bee59668e90000eb931ba9e5500c2ba07
     }
 }
