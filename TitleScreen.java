4a18717db959b16710705bf56d24d4aa41dbf64d
Vanessa-Huo <350170783@gapps.yrdsb.ca>
2024-04-08 09:13:38 -0400

    Added title and bg

--- TitleScreen.java
+++ TitleScreen.java
@@ -14,12 +14,17 @@ public class TitleScreen extends World
      * 
      */
     
+<<<<<<< Updated upstream
     Label start, label2;
+=======
+    Label title, start;
+>>>>>>> Stashed changes
     
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 800, 1); 
+<<<<<<< Updated upstream
 
         
         //Add imagebuttons NewGame and Tutorial
@@ -35,6 +40,20 @@ public class TitleScreen extends World
         {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
+=======
+        
+        title = new Label("Zoo Trip", 250);
+        addObject(title, getWidth()/2, 300);
+        start = new Label("Press 'space' to start", 80);
+        addObject(start, getWidth()/2, 550);
+    }
+    
+    
+    public void act(){
+        if(Greenfoot.isKeyDown("space")){
+            MyWorld world = new MyWorld();
+            Greenfoot.setWorld(world);
+>>>>>>> Stashed changes
         }
     }
 }
