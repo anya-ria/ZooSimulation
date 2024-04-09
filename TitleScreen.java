105c01df5d19ebc8cceb50c0c978f5fdc285e14d
Vanessa-Huo <350170783@gapps.yrdsb.ca>
2024-04-09 14:35:09 -0400

    Added subclasses under Icon

--- TitleScreen.java
+++ TitleScreen.java
@@ -13,35 +13,44 @@ public class TitleScreen extends World
      * Constructor for objects of class TitleScreen.
      * 
      */
-    
+
     Label start, achi, collec;
+    private AchieveButton achieveButton;
+    private EndButton endButton;
+    private StartButton startButton;
     
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 800, 1); 
-        
+        setBackground("title2.jpg");
 
-        achi = new Label("Achievements", 50);
-        addObject(achi, getWidth()/2, 550);
-        collec = new Label("Endings", 50);
-        addObject(collec, getWidth()/2, 650);
-    }
-    
-    public void act(){
-        if(Greenfoot.isKeyDown("space")){
-            MyWorld game = new MyWorld();
-            Greenfoot.setWorld(game);
-        }
+        startButton = new StartButton();
+        addObject(startButton,getWidth()/2,450);
         
-        if(Greenfoot.mouseClicked(achi)){
+        achieveButton = new AchieveButton();
+        addObject(achieveButton,getWidth()/2,550);
+        
+        EndButton endButton = new EndButton();
+        addObject(endButton,getWidth()/2,650);
+    }
+
+    public void act(){
+
+        if(Greenfoot.mouseClicked(endButton)){
+            Collections collec1 = new Collections();
+            Greenfoot.setWorld(collec1);
+        }
+
+        if(Greenfoot.mouseClicked(achieveButton)){
             Achievement achi1 = new Achievement();
             Greenfoot.setWorld(achi1);
         }
         
-        if(Greenfoot.mouseClicked(collec)){
-            Collections collec1 = new Collections();
-            Greenfoot.setWorld(collec1);
+        if(Greenfoot.mouseClicked(startButton)){
+            MyWorld game = new MyWorld();
+            Greenfoot.setWorld(game);
         }
     }
+
 }
