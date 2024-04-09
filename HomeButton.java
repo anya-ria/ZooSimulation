4ffd62d5906d77fa11b3e496c93807dfea0fdf22
Vanessa-Huo <350170783@gapps.yrdsb.ca>
2024-04-09 15:04:46 -0400

    Added new images and buttons

---
+++ HomeButton.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class HomeButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HomeButton extends Icon
+{
+    /**
+     * Act - do whatever the HomeButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public HomeButton(){
+        setImage("homeButton.png");
+        GreenfootImage image = getImage();
+        image.scale((int)(image.getWidth()*0.9), (int)(image.getHeight()*0.9));
+    }  
+}
