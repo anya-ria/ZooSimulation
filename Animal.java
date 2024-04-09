ce3905659a2f4c3c71db3382fcbc9da93c452791
Luke-Xiao-16 <350436366@gapps.yrdsb.ca>
2024-04-09 09:06:36 -0400

    animal update

--- Animal.java
+++ Animal.java
@@ -6,14 +6,24 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Animal extends SuperSmoothMover
+public abstract class Animal extends SuperSmoothMover
 {
     /**
      * Act - do whatever the Animal wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private boolean isInfected;
+    private double speed;
+    private double maxSpeed;
+    private int direction;
     public void act()
     {
-        // Add your action code here.
+        super.act();
+    }
+    
+    public void zombify()
+    {
+        speed = 2;
+        GreenfootImage zombie = new GreenfootImage("");
     }
 }
--- Hippo.java
+++ Hippo.java
@@ -7,14 +7,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (2024.04.05)
  * 2024.04.05: Class created
  */
+
 public class Hippo extends Animal
 {
     /**
      * Act - do whatever the Hippo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    private boolean isInfected;
+    private double speed;
+    private double maxSpeed;
+    private int direction;
+    
     public void act()
     {
-        // Add your action code here.
+        if (getOneObjectAtOffset(0, (int)(direction * getImage().getHeight()/2 + (int)(direction * speed)), Animal.class) == null)
+        {
+            setLocation (getX(), getY() + (int)(speed*direction));
+        }
+        if (direction == -1 && getY() >= 350)
+        {
+            direction = direction * -1;
+            setLocation (getX(), getY() + (int)(speed*direction));
+        }
+        else if (direction == 1 && getY() <= 5)
+        {
+            direction = direction * -1;
+            setLocation (getX(), getY() + (int)(speed*direction));
+        }
     }
 }
