8322e7235740864644d7314df19d52835a2cb72f
Vanessa-Huo <350170783@gapps.yrdsb.ca>
2024-04-09 11:48:04 -0400

    Added subclasses under Icon class

---
+++ AchieveButton.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class AchieveButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class AchieveButton extends Icon
+{
+    /**
+     * Act - do whatever the AchieveButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public AchieveButton(){
+        setImage("achievementsButton.png");
+        GreenfootImage image = getImage();
+        image.scale((int)(image.getWidth()*0.9), (int)(image.getHeight()*0.9));
+    }  
+
+}
--- Achievement.java
+++ Achievement.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.List;
+import java.util.ArrayList;
 /**
  * Write a description of class Achievement here.
  * 
@@ -13,32 +14,43 @@ public class Achievement extends World
      * Constructor for objects of class Achievement.
      * 
      */
-    
+
     Label title, achi1, achi2, achi3, achi4, achi5, achi6, returnKey;
-    
+    GoldCup[] arr = new GoldCup[6];
+
     public Achievement()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1024, 800, 1); 
-        
+
         title = new Label("Achievements", 110);
         addObject(title, getWidth()/2, 90);
         achi1 = new Label("Healer", 70);
-        addObject(achi1, 371, 200);
+        addObject(achi1, 471, 200);
         achi2 = new Label("I'm the Hospital", 70);
-        addObject(achi2, getWidth()/2, 300);
+        addObject(achi2, getWidth()/2+100, 300);
         achi3 = new Label("Banan-AHHH!", 70);
-        addObject(achi3, 481, 400);
-        achi4 = new Label("All affected", 70);
-        addObject(achi4, 443, 500);
+        addObject(achi3, 581, 400);
+        achi4 = new Label("All Affected", 70);
+        addObject(achi4, 543, 500);
         achi5 = new Label("Oops...", 70);
-        addObject(achi5, 372, 600);
+        addObject(achi5, 472, 600);
         achi6 = new Label("Zombie Zoo", 70);
-        addObject(achi6, 451, 700);
+        addObject(achi6, 551, 700);
         returnKey = new Label("Home", 50);
         addObject(returnKey, getWidth()-80, 750);
+        addCups();
     }
     
+    
+    public void addCups(){
+        for(int i=0; i<arr.length;i++){
+            arr[i] = new GoldCup();
+            addObject(arr[i], 200, (100*i+200));
+        }
+        arr[2].setAchieved();
+    }
+
     public void act(){
         if(Greenfoot.mouseClicked(returnKey)){
             TitleScreen game = new TitleScreen();
