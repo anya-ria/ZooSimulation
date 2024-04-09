64e259fd1a0fd4612a35414b21025ce301d98376
Luke-Xiao-16 <350436366@gapps.yrdsb.ca>
2024-04-09 09:22:28 -0400

    moves

--- Monkey.java
+++ Monkey.java
@@ -13,8 +13,26 @@ public class Monkey extends Animal
      * Act - do whatever the Monkey wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private boolean isInfected;
+    private double speed;
+    private double maxSpeed;
+    private int direction;
+    
     public void act()
     {
-        // Add your action code here.
+        if (getOneObjectAtOffset(0, (int)(direction * getImage().getHeight()/2 + (int)(direction * speed)), Animal.class) == null)
+        {
+            setLocation (getX(), getY() + (int)(speed*direction));
+        }
+        if (direction == -1 && getY() >= 350)
+        {
+            direction = direction * -1;
+            setLocation (getX(), getY() + (int)(speed*direction));
+        }
+        else if (direction == 1 && getY() <= 5)
+        {
+            direction = direction * -1;
+            setLocation (getX(), getY() + (int)(speed*direction));
+        }
     }
 }
