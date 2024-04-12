569691a6eef138681f28552a1dc80bcf818bd816
Anya Shah <anya.ria90210@gmail.com>
2024-04-12 09:20:26 -0400

    name

--- Penguin.java
+++ Penguin.java
@@ -3,21 +3,21 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Penguin here.
  * 
- * @author (Luke Xiao) 
+ * @author Luke Xiao, Anya Shah
  * @version (2024.04.05)
  * 2024.04.05: Class created
  */
 public class Penguin extends Animal
 {
-    /**
-     * Act - do whatever the Penguin wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     private boolean isInfected;
     private double speed;
     private double maxSpeed;
     private int direction;
     
+    /**
+     * Act - do whatever the Penguin wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
     public void act()
     {
         if (getOneObjectAtOffset(0, (int)(direction * getImage().getHeight()/2 + (int)(direction * speed)), Animal.class) == null)
