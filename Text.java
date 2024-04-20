834b4d14cace6b7b625febcf49bdbf00c3ceb974
mzqan <123510256+mzqan@users.noreply.github.com>
2024-04-20 00:22:57 -0400

    added text

---
+++ Text.java
@@ -0,0 +1,18 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Text here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Text extends Effect
+{
+    public Text(GreenfootSound sound, GreenfootImage image, int fadeIn, int duration, int fadeOut){
+        super(sound, image, fadeIn, duration, fadeOut);
+    }
+    
+    public void switchText(GreenfootImage image){
+        setImage(image);
+    }
+}
