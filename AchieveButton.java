2feb019e0e8cd9e5c34fa2ef7fe1b4d39647ff66
LucAsl14 <lucas.fu070807@gmail.com>
2024-04-22 10:35:22 -0400

    cleanup

--- AchieveButton.java
+++ AchieveButton.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class AchieveButton here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Megan Lee
+ * @version April 2024
  */
 public class AchieveButton extends Icon
 {
