86ff10a03c3a67a4a413807bc3cc8ffd7bc339b1
GennieWon <genniewon2021@gmail.com>
2024-04-25 09:29:18 -0400

    worked on sounds

--- Trophy.java
+++ Trophy.java
@@ -39,7 +39,7 @@ public class Trophy extends Icon
      */
     public void setAchieved(){
         unlock = true;
-        Greenfoot.playSound ("achievement.mp3");
+        achievement = new GreenfootSound("achievement.mp3");
         achievement.play();
         achievement.setVolume(50);
     }
