a6f4d00430c180ec2fd2039d9524b3ad9000d457
LucAsl14 <lucas.fu070807@gmail.com>
2024-04-25 11:58:21 -0400

    documented regular

--- Child.java
+++ Child.java
@@ -2,7 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * A child is an abstact class that represents all the children attending the zoo.
  * 
- * @author Lucas
+ * @author <li> Lucas | the class
+ * @author <li> Anya  | the animation stuff that Lucas brought from subclasses
  * @version 2024/4/4
  */
 public abstract class Child extends Entity
